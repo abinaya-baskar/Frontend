9038e177384ec7181c45f7ab96d9709e3ef26602
AbinayaBaskar <abinayabaskar267@gmail.com>
2025-06-14 11:09:38 +0530

    Initial commit

---
+++ Assig1.js
@@ -0,0 +1,32 @@
+let num1=5;
+let num2=3;
+add(num1, num2)
+sub(num1, num2)
+multi(num1, num2)
+div(num1, num2)
+function add(){
+    console.log(num1+ num2)
+}
+function sub(){
+    console.log(num1- num2)
+}
+function multi(){
+    console.log(num1* num2)
+}
+function div(){
+    console.log(num1/num2)
+}
+function greetUser(name){
+    console.log("Hi "+name)
+}
+greetUser("Abinaya")
+let temperature=50
+if(temperature>30){
+    console.log("Temperature is greater than 30 degree")
+}
+else{
+    console.log("Temperature is not greater than 30 degree")
+}
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
